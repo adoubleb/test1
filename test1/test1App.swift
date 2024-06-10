25d4ba2bf9f7d08327b4b5248dc14c66233ea306
adoubleb <85162617+adoubleb@users.noreply.github.com>
2024-06-10 08:55:11 +0800

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Abdul Hakam on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
